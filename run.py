78f2d77c344f914a891316a4a74926742c58fdee
pristinsky1 <44822448+pristinsky1@users.noreply.github.com>
2020-11-30 19:14:19 -0800

    Update run.py

--- run.py
+++ run.py
@@ -16,14 +16,14 @@ def main(targets):
         with open('config/test-params.json') as test_params:
             feature_cfg = json.load(test_params)
             
-            # make the data target
-            file_names, file_labels, new_df = features_labels(**feature_cfg)
-            new_df.to_csv("test/output/test_output.csv")
-            print("The associated test file names are: ") 
-            print(file_names)
-            print("The associated test file labels are: ") 
-            print(file_labels)
-            print("Created the new test features! Check folder test/output/ and observe the output features csv file!")
+        # make the data target
+        file_names, file_labels, new_df = features_labels(**feature_cfg)
+        new_df.to_csv("test/output/test_output.csv")
+        print("The associated test file names are: ") 
+        print(file_names)
+        print("The associated test file labels are: ") 
+        print(file_labels)
+        print("Created the new test features! Check folder test/output/ and observe the output features csv file!")
     return
 
     if 'result' in targets:
@@ -31,20 +31,20 @@ def main(targets):
             input_cfg = json.load(input_params)
             data_cfg = json.load(data_params)
             
-            # make the data target
-            data_names, data_labels, data_df = features_labels(**data_cfg)
-            input_names, input_labels, input_df = input_feature_label(**input_cfg)
-            final_result = ml_model_train(data_df, data_labels, input_df, input_labels)
-            print(final_result)
+        # make the data target
+        data_names, data_labels, data_df = features_labels(**data_cfg)
+        input_names, input_labels, input_df = input_feature_label(**input_cfg)
+        final_result = ml_model_train(data_df, data_labels, input_df, input_labels)
+        print(final_result)
     return
 
     if 'analysis' in targets:
         with open('config/train-data-params.json') as data_params:
             data_cfg = json.load(data_params)
             
-            # make the data target
-            data_names, data_labels, data_df = features_labels(**data_cfg)
-            prediction_labels, test_labels = ml_model_analysis(data_df, data_labels)
+        # make the data target
+        data_names, data_labels, data_df = features_labels(**data_cfg)
+        prediction_labels, test_labels = ml_model_analysis(data_df, data_labels)
     return
 
 if __name__=='__main__':
